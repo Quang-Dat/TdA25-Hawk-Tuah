b33bd2d88bdd7cc66f28d0251c2635a82f3f8742
Dat Le Quang <lequangdat06@seznam.cz>
2024-11-18 11:31:37 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/organization', function () {
+Route::get('/', function () {
     return response()->json(['organization' => 'Student Cyber Games']);
 });
